313bdf9cc61d308db75904e39bf34e09cf1db658
devops979 <devops979@gmail.com>
2025-06-12 15:02:33 +0530

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,18 +1,3 @@
-# EKS Module - Main Configuration
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
-  }
-}
-
 # Data sources
 data "aws_partition" "current" {}
 data "aws_caller_identity" "current" {}
