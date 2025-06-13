4697b30a4a2e9f83c1e80f0d0fa0ac4c47189434
devops979 <devops979@gmail.com>
2025-06-13 17:17:07 +0530

    Update main.tf

--- terraform/modules/helm/main.tf
+++ terraform/modules/helm/main.tf
@@ -1,3 +1,13 @@
+# HELM Provider
+provider "helm" {
+  kubernetes {
+    host                   = var.cluster_endpoint
+    cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+
 # Datasource: EKS Cluster Auth 
 data "aws_eks_cluster_auth" "cluster" {
   name = var.cluster_id
