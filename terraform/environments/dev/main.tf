7ed8b511a63164bb2d190739f5b53b96ae909b51
devops979 <devops979@gmail.com>
2025-06-13 10:18:19 +0530

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -151,10 +151,12 @@ module "eks" {
 
 data "aws_eks_cluster" "eks" {
   name = module.eks.eks_cluster_name
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "eks" {
   name = module.eks.eks_cluster_name
+  depends_on = [module.eks]
 }
 
 # CloudWatch Log Group for EKS
