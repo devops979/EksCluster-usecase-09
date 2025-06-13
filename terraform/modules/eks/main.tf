d2fbe630a0fe2282228ab7e79839104bd76d21d1
devops979 <devops979@gmail.com>
2025-06-13 18:29:10 +0530

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -235,7 +235,6 @@ data "tls_certificate" "cluster" {
 }
 
 resource "aws_iam_openid_connect_provider" "cluster" {
-  count = var.enable_irsa ? 1 : 0
 
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.cluster.certificates[0].sha1_fingerprint]
