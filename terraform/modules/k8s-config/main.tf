e69b09737d63bf95b62471ff2ed8665577d5b790
devops979 <devops979@gmail.com>
2025-06-13 10:42:56 +0530

    Create main.tf

---
+++ terraform/modules/k8s-config/main.tf
@@ -0,0 +1,33 @@
+# modules/k8s-config/main.tf
+provider "kubernetes" {
+  host                   = var.kube_host
+  cluster_ca_certificate = base64decode(var.kube_ca)
+  token                  = var.kube_token
+}
+
+resource "kubernetes_config_map_v1_data" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = yamlencode([
+      {
+        rolearn  = var.node_role_arn
+        username = "system:node:{{EC2PrivateDNSName}}"
+        groups   = ["system:bootstrappers", "system:nodes"]
+      }
+    ])
+
+    mapUsers = yamlencode([
+      {
+        userarn  = var.user_arn
+        username = "root"
+        groups   = ["system:masters"]
+      }
+    ])
+  }
+
+  force = true
+}
