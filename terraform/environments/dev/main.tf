0b5784a208dcbb32525197bce399a03c0fd44ab5
devops979 <devops979@gmail.com>
2025-06-13 10:46:08 +0530

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -43,14 +43,6 @@ provider "aws" {
 
 
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-  depends_on = [module.eks]
-
-}
-
 
 # Data sources
 data "aws_availability_zones" "available" {
